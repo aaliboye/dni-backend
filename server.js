ab87e67fcae19fc996c520c9d7a4a309a7e5521a
aaliboye <aalijr97@gmail.com>
2025-12-01 02:03:59 +0000

    dni don

--- server.js
+++ server.js
@@ -10,7 +10,12 @@ const app = express();
 
 // Middleware
 // Middleware
-app.use(cors())
+app.use(cors({
+  origin: "*",
+  methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+}));
+
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
