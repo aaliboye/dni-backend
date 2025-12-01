f4887ce1d0f8dfbe59f379928613cb7dd4dbc6d3
aaliboye <aalijr97@gmail.com>
2025-12-01 02:29:46 +0000

    dni don

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(cors({
   methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
 }));
-
+app.options('*', cors());
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
